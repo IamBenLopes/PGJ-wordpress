eaddb13a8030add80400d82433457c10dca3312c
Benjamin Lopes <165204265+IamBenLopes@users.noreply.github.com>
2024-10-31 23:59:57 -0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'PGJ-wordpress' ); //PGJ-wordpress
+define( 'DB_NAME', 'wordpress' ); //PGJ-wordpress
 
 /** Database username */
 define( 'DB_USER', 'root' );
